f372db05c383b8214345bccf98663eceb8901a49
Jeroen Ooms <jeroenooms@gmail.com>
2015-12-29 20:28:37 +0100

    Fix doc

--- R/webp.R
+++ R/webp.R
@@ -1,9 +1,14 @@
 #' Webp image format
 #'
-#' Read and write webp images into a raw bitmap array.
+#' Read and write webp images into a bitmap array. The bitmap array uses the same
+#' conventions as the \code{png} and \code{jpeg} package.
 #'
 #' @export
 #' @useDynLib webp R_webp_decode
+#' @rdname read_webp
+#' @param source raw vector or path to webp file
+#' @param numeric convert the image to 0-1 real numbers to be compatible with
+#' images from the jpeg or png package.
 #' @examples # Convert to webp
 #' library(png)
 #' img <- readPNG(system.file("img", "Rlogo.png", package="png"))
@@ -16,11 +21,11 @@
 #' img <- read_webp("rlogo.webp")
 #' writeJPEG(img, "rlogo.jpeg")
 #' browseURL("rlogo.jpeg")
-read_webp <- function(buf, numeric = TRUE) {
-  if(is.character(buf))
-    buf <- readBin(buf[1], raw(), file.info(buf)$size)
-  stopifnot(is.raw(buf))
-  out <- .Call(R_webp_decode, buf)
+read_webp <- function(source, numeric = TRUE) {
+  if(is.character(source))
+    source <- readBin(source[1], raw(), file.info(source)$size)
+  stopifnot(is.raw(source))
+  out <- .Call(R_webp_decode, source)
   if(isTRUE(numeric)){
     out <- structure(as.numeric(out)/255, dim = dim(out))
     out <- aperm(out)
@@ -31,17 +36,22 @@ read_webp <- function(buf, numeric = TRUE) {
 }
 
 #' @export
+#' @rdname read_webp
 #' @useDynLib webp R_webp_encode
-write_webp <- function(img, file = NULL, quality = 80) {
-  if(is.numeric(img)){
-    img <- structure(as.raw(img * 255), dim = dim(img))
-    img <- aperm(img)
+#' @param numeric array of 3 dimensions (width * height * channel) with real numbers
+#' between 0 and 1.
+#' @param target path to a file or \code{NULL} to return the image as a raw vector
+#' @param quality value between 0 and 100
+write_webp <- function(image, target = NULL, quality = 80) {
+  if(is.numeric(image)){
+    image <- structure(as.raw(image * 255), dim = dim(image))
+    image <- aperm(image)
   }
-  channels = dim(img)[1]
+  channels = dim(image)[1]
   stopifnot(channels == 3 || channels == 4)
-  buf <- .Call(R_webp_encode, img, quality)
-  if(is.character(file))
-    writeBin(buf, file)
+  buf <- .Call(R_webp_encode, image, quality)
+  if(is.character(target))
+    writeBin(buf, target)
   else
     structure(buf, class = "webp")
 }
